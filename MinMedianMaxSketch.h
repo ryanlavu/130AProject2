a83c57c001bd8157280a608fab8e736af54b982a
ryanlavu <ryanvu@ucsb.edu>
2021-11-03 14:45:22 -0700

    asd

--- MinMedianMaxSketch.h
+++ MinMedianMaxSketch.h
@@ -3,3 +3,38 @@
 
 using namepsace std;
 
+class MinMedianMaxSketch
+{
+	private:
+		int size;
+	public:
+		MinMedianMaxSketch()
+		{
+		}
+
+		insert(int newElement)
+		{
+		}
+
+		remove(int goneElement)
+		{
+		}
+
+		get_median()
+		{}
+
+		get_minimum()
+		{}
+
+		get_maximum()
+		{
+		}
+
+		get_size()
+		{
+		}
+
+		search(int findElement)
+		{}
+
+}

04e88e8cfb0e762c0fa8e099959072c19b2d7d75
ryanlavu <ryanvu@ucsb.edu>
2021-11-12 00:17:26 -0800

    test

--- Heap.cpp
+++ Heap.cpp
@@ -1,41 +1,43 @@
-#include "Heap.h"
+
 
 #include <vector>
 #include <iostream>
 
-using namespace std;
-/* Constructor
- * bool isMax: true = max heap, false = min heap
- */
-template <class T>
-Heap::Heap(bool isMax) {
+#include <string>
 
+using namespace std;
+
+template<class T>
+class Heap {
+
+	public:
+
+	/* Constructor
+	* bool isMax: true = max heap, false = min heap
+	*/
+	Heap(bool isMax) {
 	max = isMax;
 	size = 0;
 	heap = new vector <T>();
+	}
 
-}
-
-/* Insert function
- * int element: element you want to insert into heap
- */
-template <class T>
-void Heap::insert(T element) {
-	
+	/* Insert function
+	* int element: element you want to insert into heap
+	*/
+	void insert(T element) {
 	// Insert element into the last slot of tree
 	heap.push_back(element);
 
 	// Function that bubbles up the last element
 	orderLast();
 	setSize(getSize() + 1);
+	}
 
-}
+	/* Remove function
+	* int target: element you want to remove from heap
+	*/
 
-/* Remove function
- * int target: element you want to remove from heap
- */
-template <class T>
-void Heap::remove(T target) {
+void remove(T target) {
 
 	int targetIndex = -1;
 
@@ -134,47 +136,42 @@ void Heap::remove(T target) {
 
 }
 
-/* Get root function
- * return: int value in the root
- */
-template <class T>
-T Heap:getRoot() {
+		/* Get root function
+		* return: int value in the root
+		*/
+		T getRoot() {
+		// Root should be at 0 index of the vector
+		return heap[0];
+		}
 
-	// Root should be at 0 index of the vector
-	return heap[0];
+		/* Extract root function
+		* return: int value in the root
+		*/
+		T extractRoot() {
 
-}
+		// Save value of root in order to not lose when removing
+		T value = getRoot();
 
-/* Extract root function
- * return: int value in the root
- */
-template <class T>
-T Heap::extractRoot() {
+		// Call removed function targeting the value of the root
+		remove(value);
 
-	// Save value of root in order to not lose when removing
-	T value = getRoot();
+		// Return the saved value
+		return value;
 
-	// Call removed function targeting the value of the root
-	remove(value);
+		}
 
-	// Return the saved value
-	return value;
+		/* Get minimum function
+		* return: int value of smallest value
+		*/
+		T getMin() {
 
-}
+		// If min heap then simply return the root
+		if(!max) return getRoot();
 
-/* Get minimum function
- * return: int value of smallest value
- */
-template <class T>
-T Heap::getMin() {
+		T min = heap[0];
 
-	// If min heap then simply return the root
-	if(!max) return getRoot();
-
-	T min = heap[0];
-
-	// If max heap then iterate through the entire vector
-	for(int i = 1; i < heap.size(); i++) {
+		// If max heap then iterate through the entire vector
+		for(int i = 1; i < heap.size(); i++) {
 
 		if(heap[i] < min) min = heap[i];
 
@@ -182,55 +179,51 @@ T Heap::getMin() {
 
 }
 
-/* Get maximum function
- * return: int value of largest value
- */
-template <class T>
-T Heap::getMax() 
+	/* Get maximum function
+	* return: int value of largest value
+	*/
 
-	// If max heap then simply return the root
-	if(max) return getRoot();
+	T getMax() {
 
-	T max = heap[0];
+		// If max heap then simply return the root
+		if(max) return getRoot();
 
-	// If min heap then iterate through the entire vector
-	for(int i = 1; i < heap.size(); i++) {
+		T max = heap[0];
 
-		if(heap[i] > max) max = heap[i];
+		// If min heap then iterate through the entire vector
+		for(int i = 1; i < heap.size(); i++) {
 
-	}
+			if(heap[i] > max) max = heap[i];
 
-}
-
-/* Get size function
- * return: int value of heap size
- */
-//template <class T>
-//int Heap::getSize() { return size; }
-
-/* Search function
- * int target: value of element you want to find
- * return: boolean if element is found
- */
-template <class T>
-bool Heap::search(T target) {
-
-	// Iterate through the entire vector in order to find target element
-	for(int i = 0; i < heap.size(); i++) {
-
-		if(heap[i] == target) return true;
+		}
 
 	}
 
-	return false;
 
+		int getSize(){return size;}
+
+
+		/* Search function
+		* int target: value of element you want to find
+		* return: boolean if element is found
+		*/
+
+		bool search(T target) {
+
+		// Iterate through the entire vector in order to find target element
+		for(int i = 0; i < heap.size(); i++) {
+
+			if(heap[i] == target) return true;
+
+		}
+
+		return false;
 }
 
-/* Order last function
+		/* Order last function
  * Bubbles up the last element in vector to sort
  */
-template <class T>
-void Heap::orderLast() {
+void orderLast() {
 
 	// Starting at the last element compare with it's parent nodes
 	for(int i = heap.size(); i > 0; i = ((i - 1) / 2)) {
@@ -252,3 +245,15 @@ void Heap::orderLast() {
 	}
 
 }
+
+		void setSize(int setTo){size = setTo;}
+		
+	private:
+		bool max;
+		vector<T> heap;
+		int size;
+};
+
+
+
+
